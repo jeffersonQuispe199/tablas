904fcff2dad90c7facfba122a641c2c534fac976
JEFFERON QUISPE <jeffersonquispecando@gmail.com>
2026-04-28 16:00:38 -0500

    modificar el codigo para la tabla del 5

--- multiplicar.js
+++ multiplicar.js
@@ -3,7 +3,7 @@ function generarTablas(){
     //contenedor.innerHTML = " <h1> Probando </h1>";
     let contenido = " ";
    
-    let tabla = 3;
+    let tabla = 5;
     for (let i = 1; i <= 10; i++) {
         // Calculamos el resultado en cada vuelta
         let resultado = tabla * i;
